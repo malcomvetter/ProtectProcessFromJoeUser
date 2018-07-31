c8f211c19124de377ad4cc236b8f7e73cccbd9b0
malcomvetter <malcomvetter@gmail.com>
2018-07-31 12:02:17 -0500

    init

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProtectProcess.ProtectProcess.Protect();
+            Console.WriteLine("You can't kill me. Only an admin can kill me...");
+#if DEBUG
+            Console.ReadLine();
+#endif
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# ProtectProcessFromJoeUser
+
+Build in Visual Studio (.net 3.5 so it runs on Win 7+). Fire up as a regular user, it will strip permissions off the process to prevent a user from killing the process. Admins, of course, can still kill the process.
